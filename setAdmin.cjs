32e182c7cd9bc05afd44268017c6abc467fee866
chandrakantteotia <chandrakantteotia@example.com>
2025-07-11 21:51:06 +0530

    Removed setAdmin script after initial setup

--- setAdmin.cjs
+++ setAdmin.cjs
@@ -1,21 +1,21 @@
-const admin = require('firebase-admin');
-// const serviceAccount = require('./serviceAccountKey.json');
-const serviceAccount = require('/etc/secrets/serviceAccountKey.json'); 
+// const admin = require('firebase-admin');
+// // const serviceAccount = require('./serviceAccountKey.json');
+// const serviceAccount = require('/etc/secrets/serviceAccountKey.json'); 
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-const uid = 'f4LDhgwfLGblAU98AkROdCuTY783';
+// const uid = 'f4LDhgwfLGblAU98AkROdCuTY783';
 
-admin
-  .auth()
-  .setCustomUserClaims(uid, { admin: true })
-  .then(() => {
-    console.log('✅ Admin claim set successfully for UID:', uid);
-    process.exit(0);
-  })
-  .catch((error) => {
-    console.error('❌ Error setting admin claim:', error);
-    process.exit(1);
-  });
+// admin
+//   .auth()
+//   .setCustomUserClaims(uid, { admin: true })
+//   .then(() => {
+//     console.log('✅ Admin claim set successfully for UID:', uid);
+//     process.exit(0);
+//   })
+//   .catch((error) => {
+//     console.error('❌ Error setting admin claim:', error);
+//     process.exit(1);
+//   });
